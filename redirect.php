5427f54ac8a11ea320745b6595c15b944e962d9d
kelvinjustin <knthinda@gmail.com>
2026-07-25 23:06:01 +0200

    Hardcode server IP to 192.168.1.164 since SERVER_ADDR returns 127.0.0.1 on local XAMPP

--- redirect.php
+++ redirect.php
@@ -10,8 +10,9 @@ $query_string = $_SERVER['QUERY_STRING'] ?? '';
 // Use HTTP for local server (HTTPS to HTTP downgrade)
 $protocol = "http://";
 
-// Get the actual server IP
-$server_ip = $_SERVER['SERVER_ADDR'] ?? $_SERVER['LOCAL_ADDR'] ?? '192.168.1.165';
+// Get the actual server IP - use 192.168.1.164 as it's the actual network IP
+// SERVER_ADDR returns 127.0.0.1 on local XAMPP
+$server_ip = '192.168.1.164';
 
 // Build the redirect URL with explicit slash
 $redirect_url = $protocol . $server_ip . '/';
