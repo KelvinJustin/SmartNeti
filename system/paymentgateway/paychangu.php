ce7cb0b3e0e6d21acd0fc679c9ca7cdc99dcf04a
kelvinjustin <knthinda@gmail.com>
2026-07-25 23:15:45 +0200

    Use redirect.php for both callback_url and return_url since PayChangu callback is a user redirect, not just webhook

--- system/paymentgateway/paychangu.php
+++ system/paymentgateway/paychangu.php
@@ -116,10 +116,9 @@ function paychangu_create_transaction($trx, $user)
   // Generate unique transaction reference
   $tx_ref = 'INV-' . $trx['id'] . '-' . time();
 
-  // Use redirect.php for return_url (user-facing redirect)
-  // Use direct ngrok URL for callback_url (server-to-server webhook)
+  // Use redirect.php for both callback_url and return_url (both are user-facing redirects)
+  // Webhook is a separate server-to-server call handled by PayChangu
   $redirect_url = $config['paychangu_return_url'];
-  $callback_url = $config['paychangu_callback_url'];
 
   $url = 'https://api.paychangu.com/payment';
 
@@ -129,7 +128,7 @@ function paychangu_create_transaction($trx, $user)
     'email' => $user['email'] ?: '',
     'first_name' => $user['fullname'] ? explode(' ', $user['fullname'])[0] : '',
     'last_name' => $user['fullname'] ? (count(explode(' ', $user['fullname'])) > 1 ? implode(' ', array_slice(explode(' ', $user['fullname']), 1)) : '') : '',
-    'callback_url' => $callback_url,
+    'callback_url' => $redirect_url . '?_route=callback/paychangu',
     'return_url' => $redirect_url . '?_route=order/view/' . $trx['id'],
     'tx_ref' => $tx_ref,
     'customization' => [
